b0a1f4e87aee5ef3b31e20fa24f28013807fee48
jeseo1105-eng <jeseo1105@gmail.com>
2025-11-23 01:45:51 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="다중이용업소 통계 대시보드",
+    page_icon="🔥",
+    layout="wide",
+)
+
+st.title("🔥 전국 다중이용업소 통계 대시보드")
+st.write(
+    """
+    왼쪽 사이드바에서 Pages 메뉴로 이동해
+    업종별 통계 그래프를 볼 수 있습니다.
+    """
+)
